a5f0099ec43a1499863fea34a9bb59eaa2ae62d1
pauloqxm <pauloqxm@gmail.com>
2025-08-24 20:10:48 -0300

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -186,7 +186,7 @@ def render_header():
         <div class="custom-header">
         <div class="header-container">
             <div class="header-brand">
-            <img src="https://i.ibb.co/KpHGQ6qg/LOGOPROTAL.png" class="header-logo">
+            <img src="https://i.ibb.co/KpHGQ6qg/LOGOPROTAL.png" class="header-logo" style="width: 50px; height: auto;">
             <div>
                 <div class="header-title">Transparência e Participação</div>
                 <div style="opacity:.9;font-size:13px">📌 Portal do Comitê da Sub-Bacia do Rio Banabuiu</div>
@@ -412,6 +412,7 @@ def salvar_em_planilha(dados_formulario):
     except Exception as e:
         st.error(f"Erro ao salvar na planilha: {e}")
         return False
+
 
 
 
