406285956c550afc9811a5ba5192880b9fc911c5
pauloqxm <pauloqxm@gmail.com>
2025-08-26 11:22:21 -0300

    Update acudes.py

--- pages/acudes.py
+++ pages/acudes.py
@@ -227,7 +227,7 @@ def render_acudes():
             MousePosition(position="bottomleft").add_to(m)
 
             # ✅ usar st_folium com altura fixa (sem width em string)
-            st_folium(m, height=520)
+            folium_static(m, width='100%', height=500)
         else:
             st.warning("Não há reservatórios com os filtros aplicados.")
 
@@ -335,3 +335,4 @@ def render_acudes():
                 )
         else:
             st.warning("⚠️ Nenhum dado encontrado com os filtros aplicados.", icon="⚠️")
+
