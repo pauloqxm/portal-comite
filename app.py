c4b31aaddf3530efae2f517d8698959499a575f0
pauloqxm <pauloqxm@gmail.com>
2025-08-27 13:35:00 -0300

    13:35 app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pages import home, acudes, docs, dados, vazoes_dashboard, fale_conosco, o_comite
+from pages import home, acudes, docs, dados, vazoes_dashboard, fale_conosco, o_comite, publicacoes
 from utils.common import render_header, render_footer
 
 # ---------------- CONFIG GERAL ----------------
@@ -16,7 +16,7 @@ render_header()
 # =========================
 # CRIAÇÃO DAS ABAS
 # =========================
-tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["🏠 Inicial", "💧 Painel da Operação", "🗺️ Açudes Monitorados", "📈 Situação das Sedes", "💬 Alocação Negociada", "✉️ Fale Conosco", "🙋🏽 O Comitê"])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["🏠 Inicial", "💧 Painel da Operação", "🗺️ Açudes Monitorados", "📈 Situação das Sedes", "💬 Alocação Negociada", "✉️ Fale Conosco", "🙋🏽 O Comitê", "📚 Publicações/Acervo"])
 
 with tab1:
     home.render_home()
@@ -39,8 +39,12 @@ with tab6:
 with tab7:
     o_comite.render_o_comite()
 
+with tab8:
+    publicacoes.render_publicacoes()
+
 # ======================RODAPÉ (GLOBAL)
 render_footer()
+
 
 
 
