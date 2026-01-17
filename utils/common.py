0afd35873e0ec0e42123ff93297b8e86df4834d2
pauloqxm <pauloqxm@gmail.com>
2026-01-17 18:31:44 -0300

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -217,13 +217,6 @@ def render_header():
         """,
         unsafe_allow_html=True,
     )
-def inject_gptmaker_widget():
-    st.markdown(
-        """
-        <script async src="https://app.gptmaker.ai/widget/3ED61474B48BE397410802603320372A/float.js"></script>
-        """,
-        unsafe_allow_html=True
-    )
 
 def render_footer():
     """Renderiza o rodapé da aplicação."""
@@ -419,6 +412,7 @@ def salvar_em_planilha(dados_formulario):
     except Exception as e:
         st.error(f"Erro ao salvar na planilha: {e}")
         return False
+
 
 
 
